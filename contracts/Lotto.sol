8a4b4edb42708a650d32ab2b82dc1caff2181b67
Javel Wilson <javelawilson@gmail.com>
2020-10-08 22:30:56 -0500

    Define functions to pick winner and get previous winners

--- contracts/Lotto.sol
+++ contracts/Lotto.sol
@@ -4,6 +4,7 @@ contract Lotto {
     address public manager;
     uint public entryFee;
     address[] players;
+    address[] previousWinners;
 
     constructor(uint amount) public {
         require(amount * 1 ether >= 1 ether);
@@ -19,5 +20,21 @@ contract Lotto {
 
     function getPlayers() public view returns (address[]) {
         return players;
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager);
+        uint index = 0;
+        address winner = players[index];
+        previousWinners.push(winner);
+        players = new address[](0);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, manager, players, previousWinners))) % players.length;
+    }
+
+    function getPreviousWinners() public view returns (address[]) {
+        return previousWinners;
     }
 }
